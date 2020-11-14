c1e7cbdc3476162016cf76ba55ea47e7c8f3a001
Marjori Fonte <marjorif@live.com>
2020-11-14 17:01:17 +0000

    Beers detail WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require "open-uri"
+
 puts "destroying beers"
 Beer.destroy_all
 
-Beer.create(name:'Dois Corvos', category: 'Blonde Ale', alcohol:'4', ibu: 3, temperature: 13, description: 'So good')
-Beer.create(name:'8ª Colina', category: 'IPA', alcohol:'5.1', ibu: 4, temperature: 13, description: 'So awesome')
+beer1 = Beer.create(name:'Dois Corvos', category: 'Blonde Ale', alcohol:'4', ibu: 3, temperature: 13, description: 'So good')
+beer2 = Beer.create(name:'8ª Colina', category: 'IPA', alcohol:'5.1', ibu: 4, temperature: 13, description: 'So awesome')
+beer3 = Beer.create(name:'Coruja', category: 'Amber Lager', alcohol:'3', ibu: 2, temperature: 10, description: 'Incredible taste')
 
 puts "beers created"
+
+
