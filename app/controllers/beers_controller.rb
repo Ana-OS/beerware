9c6bdfdbf294eb5c92633caf723f9686933fd025
ana sanots <anasoliveiraaa@gmail.com>
2020-11-16 11:24:43 +0000

    beers index

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,6 +1,4 @@
 class BeersController < ApplicationController
-  before_action :find_beer, except: [:index]
-
   def index
     if params[:query].present?
       @beers = Beer.search_by_name_category_description(params[:query])
@@ -9,34 +7,7 @@ class BeersController < ApplicationController
     end
   end
 
-  def show;end
-
-  # def new
-  #   @beer = Beer.new
-  # end
-
-  # def create
-  #   # @bar = Bar.find(params[:bar_id])
-  #   @beer = Beer.new(beer_params)
-  #   # @beer.bar = @bar
-
-  #   if @beer.save
-  #     redirect_to beers_path
-  #   else
-  #     render :new
-  #   end
-  # end
-
-  # def destroy;end
-
-
-  private
-
-  def find_beer
+  def show
     @beer = Beer.find(params[:id])
-  end
-
-  def beer_params
-    params.require(:beer).permit(:name, :category, :alcohol, :ibu, :temperature, :description)
   end
 end
