492287f3ddfdc82cfc62d6587250fdc9798e6845
Claudine McGowan <claudine.mcgowan@gmail.com>
2020-11-17 19:41:29 +0000

    bars controller and index page

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,4 +1,12 @@
 class Bar < ApplicationRecord
   has_many :bars_beers
   has_many :beers, through: :bars_beers
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_location_description,
+   against: [:name, :location],
+
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
