0fe159ded46389e0cb266f5b234258cf60e35905
ana sanots <anasoliveiraaa@gmail.com>
2020-11-23 13:00:46 +0000

    linked some beers and bars name to their show page

--- app/javascript/components/map.js
+++ app/javascript/components/map.js
@@ -7,12 +7,12 @@ const initMapbox = () => {
   const fitMapToMarkers = (map, markers) => {
 
     const bounds = new mapboxgl.LngLatBounds();
-
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
     map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   };
 
   if (mapElement) { // only build a map if there's a div#map to inject into
+
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
@@ -22,7 +22,7 @@ const initMapbox = () => {
     const markers = JSON.parse(mapElement.dataset.markers);
 
     markers.forEach((marker) => {
-      const popup = new mapboxgl.Popup({closeOnClick: true, offset: [50, 40]}).setHTML(marker.infoWindow);
+      const popup = new mapboxgl.Popup({closeOnClick: true}).setHTML(marker.infoWindow);
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup)
@@ -30,6 +30,7 @@ const initMapbox = () => {
     });
 
     fitMapToMarkers(map, markers);
+
     map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
                                       mapboxgl: mapboxgl }));
   }
